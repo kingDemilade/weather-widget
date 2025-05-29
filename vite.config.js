45a63190d67f60078c6e7ec315a012e29221a736
DMAC - Public Affairs <publicaffairs@PAD-MN7H9FHVKX.local>
2025-05-29 00:48:34 -0400

    Final weather app changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/weather-widget/', // <- MUST match the GitHub repo name!
+  plugins: [react()]
 })
